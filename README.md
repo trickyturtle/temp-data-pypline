21f416c27e847e8b7ea9f50126379bf1c29ef758
David Alexander <dralexander08@gmail.com>
2020-10-08 11:01:33 -0500

    finalize sensor

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # temp-data-pypline
-A temp data pipeline written in python
+A temperature data pipeline written in python using postgres in a docker container
+
+To create the postgres docker container use:
+`docker run -p 5432:5432 --name DATABASE_NAME -e POSTGRES_PASSWORD=postgres postgres`
+
+To monitor, I also spun up a pgadmin docker container like so:
+1. `docker run -p 5555:80 --name pgadmin -e PGADMIN_DEFAULT_EMAIL="YOUR_EMAIL@HOTMAIL.COM" -e PGADMIN_DEFAULT_PASSWORD="password" dpage/pgadmin4`
+
+2. Sign into pgadmin using the credentials I set and connect it to the postgres docker container
--- unit_tests.py
+++ unit_tests.py
@@ -12,19 +12,12 @@ import data_processor
 import data_enricher
 import data_pipeline
 import uuid
-import json
 
-LOG_FILE_A="tempSensorLogA.json"
-LOG_FILE_B="tempSensorLogB.json"
 
 class TestTempDataPipeline(unittest.TestCase):
     # =============================================================================
     # Sensor Tests
     # =============================================================================
-
-    #Test that sensor returns a float temp
-    def test_getTemp(self):
-        self.assertTrue(isinstance(sensor.getTemp(), int))
 
     #Test that sensor.getTempData returns a dict with the correct fields
     def test_getTempData(self):
@@ -45,34 +38,7 @@ class TestTempDataPipeline(unittest.TestCase):
         self.assertRegex( tempData["content"]["time_of_measurement"],
                          '\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d')
         
-    #Test that runSensor generates logs correctly
-    def test_generateSensorLog(self):
-        sensor.clearLogFile(LOG_FILE_A)
-        sensor.clearLogFile(LOG_FILE_B)
-        sensor.generateSensorLog(2*sensor.LOG_MAX+2, True)
-        with open(LOG_FILE_A, "r") as readFile:
-            logs = readFile.read().splitlines()
- 
-        #Verify old log is deleted on reaching max number of entries
-        self.assertTrue(len(logs)==2)
-    
-        for log in logs:
-            log = json.loads(log)
-            self.assertTrue(isinstance(log, dict))
-            self.assertTrue("id" in log)
-            self.assertTrue("type" in log)
-            self.assertTrue("content" in log)
-            try:
-                self.assertTrue(isinstance(uuid.UUID(log["id"]), uuid.UUID))
-            except ValueError:
-                self.fail("Data did not have a valid UUID")
-            self.assertTrue(log["type"]=="sensor")
-            self.assertTrue("temperature_f" in log["content"])
-            self.assertTrue("time_of_measurement" in log["content"])
-            self.assertTrue(isinstance(log["content"]["temperature_f"], int))
-            self.assertTrue(isinstance(log["content"]["time_of_measurement"], str))
-            self.assertRegex( log["content"]["time_of_measurement"],
-                             '\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d')
+   
         
 
 if __name__ == '__main__':
