5f982d1d387609a762fc85905e8edd0ef67c58bb
David Alexander <dralexander08@gmail.com>
2020-10-08 12:58:29 -0500

    update unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -10,8 +10,14 @@ import unittest
 import sensor
 import data_processor
 import data_enricher
-import data_pipeline
+import psycopg2
+import multiprocessing as mp
 import uuid
+
+PG_HOST_NAME="localhost"
+PG_DATA_BASE="postgres"
+PG_USER_NAME="postgres"
+PG_PASSWORD="postgres"
 
 
 class TestTempDataPipeline(unittest.TestCase):
@@ -38,8 +44,59 @@ class TestTempDataPipeline(unittest.TestCase):
         self.assertRegex( tempData["content"]["time_of_measurement"],
                          '\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d')
         
-   
+    # =============================================================================
+    # Data Enricher Tests
+    # =============================================================================
+    #Test that the data_enricher produces a properly formatted message
+    def test_enrichAndEnqueDataMessage(self):
+        queue = mp.JoinableQueue()
+        data_enricher.enrichAndEnqueDataMessage(sensor.getTempData(), queue)
+        msg = queue.get()
+        try:
+            self.assertTrue(isinstance(uuid.UUID(msg[0]), uuid.UUID))
+        except ValueError:
+            self.fail("Data did not have a valid UUID")
+        self.assertTrue(msg[1]=="sensor")
+        self.assertTrue(isinstance(msg[2], int))
+        self.assertTrue(isinstance(msg[3], float) or isinstance(msg[3], int))
+        self.assertRegex(msg[4],
+                         '\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d')
+ 
+    # =============================================================================
+    # Data Processor Tests
+    # =============================================================================
+    #Test that the data_processor correctly inputs data into the db
+    def test_insertDataFromQueue(self):
+        data_processor.clearTable()
+        data_processor.createTable()
+        queue = mp.JoinableQueue()
+        for i in range(3):
+            data_enricher.enrichAndEnqueDataMessage(sensor.getTempData(), queue)
+        worker_process = mp.Process(target=data_processor.insertDataFromQueue, args=(queue, ), daemon=True, name='worker_process_{}'.format(i))
+        worker_process.start()
+        queue.join()
+        conn = psycopg2.connect(host=PG_HOST_NAME,
+                                    database=PG_DATA_BASE,
+                                    user=PG_USER_NAME,
+                                    password=PG_PASSWORD)
+        cur = conn.cursor()
+        cur.execute("select * from temperatures")
+        contents = cur.fetchall()
+        i=0
+        for msg in contents:
+            i+=1
+            try:
+                self.assertTrue(isinstance(uuid.UUID(msg[0]), uuid.UUID))
+            except ValueError:
+                self.fail("Data did not have a valid UUID")
+            self.assertTrue(msg[1]=="sensor")
+            self.assertTrue(isinstance(msg[2], int))
+            self.assertTrue(isinstance(msg[3], float) or isinstance(msg[3], int))
+            self.assertRegex(msg[4],
+                             '\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d')
+        self.assertTrue(i==3)
+        cur.close()
+        conn.close()
         
-
 if __name__ == '__main__':
     unittest.main()
