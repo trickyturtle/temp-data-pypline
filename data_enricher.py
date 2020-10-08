316448ccc68d43a1b4f47a755f53fef3d3fc805b
David Alexander <dralexander08@gmail.com>
2020-10-08 12:57:25 -0500

    implement data_enricher

--- data_enricher.py
+++ data_enricher.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct  4 15:50:29 2020
-
 @author: drale
+A data enricher that takes data from a sensor
 """
+import sys
+import logging
 
-
+def enrichAndEnqueDataMessage(data, queue):
+    temperature_c = (data["content"]["temperature_f"] - 32) * (5.0/9.0)
+    try:
+        msg = [data["id"], data["type"], data["content"]["temperature_f"], 
+            temperature_c, data["content"]["time_of_measurement"]]
+        queue.put(msg)
+    except:
+        #This is probably due to a malformed data message. A single data
+        #point is probably less valuable than keeping the pipeline moving, so
+        #we note it and move on.
+        logging.error("Unexpected error:", sys.exc_info()[0])
+    return msg
